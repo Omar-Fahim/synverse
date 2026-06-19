e9dfb5ab702b282a60998d140f394bf541df164f
Omar-Fahim <ofahim2002@gmail.com>
2026-06-20 01:59:24 +0200

    Returned both work and results paths to work. Clusters can not handle passing files from one dirctory to another

--- scripts/run_synverse.sh
+++ scripts/run_synverse.sh
@@ -43,7 +43,7 @@ nextflow run . \
   -profile conda,gpu \
   --input $RESOLVED_CFG \
   -work-dir $WORK/synverse/regular_work_a100 \
-  --outdir $HPCVAULT/synverse/regular_results_a100_2
+  --outdir $WORK/synverse/regular_results_a100
 
 EXIT_CODE=$?
 echo "Pipeline finished at $(date)"
