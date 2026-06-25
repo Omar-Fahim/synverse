80eb3316c209542a9ef003112a4eb9ccead1f2ed
Omar-Fahim <ofahim2002@gmail.com>
2026-06-25 16:27:12 +0200

    - Changed the code to use 2 GPUs - Tried to fix any issues that may cause erros in GPU search

--- scripts/run_synverse.sh
+++ scripts/run_synverse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH --job-name=synverse_a100_regular_1_GPUs
+#SBATCH --job-name=synverse_a100_regular_2_GPUs
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --partition=a100
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100:2
 # SBATCH --partition=v100
 # SBATCH --gres=gpu:v100:1
 #SBATCH --ntasks=1
@@ -46,8 +46,8 @@ nextflow run . \
   -resume \
   -profile conda,gpu \
   --input $RESOLVED_CFG \
-  -work-dir $HPCVAULT/synverse/regular_work \
-  --outdir $HPCVAULT/synverse/regular_results \
+  -work-dir $WORK/synverse/regular_work \
+  --outdir $WORK/synverse/regular_results \
   -c conf/cluster_gpu.config \
   -with-report report_${SLURM_JOB_ID}.html \
   -with-trace trace_${SLURM_JOB_ID}.txt \
