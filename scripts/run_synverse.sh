2acd961405aec0e3db96daeec6c2378115127900
Omar-Fahim <ofahim2002@gmail.com>
2026-06-28 01:37:22 +0200

    - Changed code to use HPCVAULT for results and work directories.
    - Changed gpu_locks directory to use vault
    instead of work.

--- scripts/run_synverse.sh
+++ scripts/run_synverse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=synverse_a100_regular_2_GPUs
+#SBATCH --job-name=synverse_a100_regular_2_GPUs_cv
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 #SBATCH --time=24:00:00
@@ -45,8 +45,8 @@ echo "[$(date +%H:%M:%S)] Starting Nextflow..."
 nextflow run . \
   -profile conda,gpu \
   --input $RESOLVED_CFG \
-  -work-dir $WORK/synverse/regular_work \
-  --outdir $WORK/synverse/regular_results \
+  -work-dir $HPCVAULT/synverse/regular_work \
+  --outdir $HPCVAULT/synverse/regular_results \
   -c conf/cluster_gpu.config \
   -with-report report_${SLURM_JOB_ID}.html \
   -with-trace trace_${SLURM_JOB_ID}.txt \
